510aae56541c809a10b308bed6954d790279696f
oasisks <oasisks2@gmail.com>
2024-12-03 17:08:26 -0500

    Finished the function that grabs the time series data

--- src/app.py
+++ src/app.py
@@ -1,6 +1,42 @@
+import time
+
 import streamlit as st
 from rag import get_summary
-from llm.summary import list_batches
+from llm.summary import list_batches, retrieve_batch_summarize, SummaryStatus
+
+
+@st.cache_data
+def get_batches():
+    return list_batches()
+
+
+def side_bar():
+    # adding the batches
+    st.sidebar.title("View Batch Summaries")
+    batches = {batch[1]: batch[0] for batch in get_batches()}
+
+    menu = st.sidebar.selectbox("Select a batch", [""] + [batch for batch in batches])
+
+    # this will just show the batch results and its summaries
+    if menu and not menu.isspace():
+        status, response = retrieve_batch_summarize(batches[menu])
+        st.sidebar.write("# Retrieved Summaries")
+        st.sidebar.write(f"## Status : {status}")
+        for line in response:
+            st.sidebar.write(f"### For {line['custom_id'].split('_')[0].upper()}")
+            items = line["response"]["body"]["choices"][0]["message"]["content"].split("\n")
+            i = 1
+            for item in items:
+                if item.startswith("Tweet") or item.startswith("Article"):
+                    st.sidebar.write(f"{i}. {item}")
+                    i += 1
+                else:
+                    st.sidebar.text(item)
+
+
+def main():
+    pass
+
 
 st.set_page_config(
     page_title="Non-Collapsible Sidebar",
@@ -9,38 +45,55 @@ st.set_page_config(
     initial_sidebar_state="expanded"  # Keep the sidebar expanded and non-collapsible
 )
 
+side_bar()
+
 st.title("Stock Price Prediction")
 
-# adding the batches
-st.sidebar.title("Scrollable Sidebar")
-batches = list_batches()
-
-menu = st.sidebar.selectbox("Select an option", ["Home", "About", "Contact"])
-
 # Add some input for the user
-tickers = st.text_input("Enter stock symbols separated by commas (i.e. AAPL, GOOG)", "AAPL").split(",")
-
+tickers = st.text_input("Enter stock symbols separated by commas (i.e. AAPL, GOOG)")
 is_valid = True
-for ticker in tickers:
-    if not ticker:
-        is_valid = False
 
-# Create buttons in the sidebar
-if st.sidebar.button("Button 1"):
-    st.write("You clicked Button 1!")
+if tickers:
+    tickers = tickers.split(",")
+    for ticker in tickers:
+        if not ticker:
+            is_valid = False
+        if ticker.isspace():
+            is_valid = False
+else:
+    is_valid = True
 
-if st.sidebar.button("Button 2"):
-    st.write("You clicked Button 2!")
-# for i in range(1, 101):  # Simulating a large number of items
-#     st.sidebar.write(f"Item {i}")
-### Context Retriver ###
-# Rag component will grab the last 24 hours of tweets for the given ticker
-# Rag component will grab the last 24 hours of news for the given ticker
+if is_valid:
+    ### Context Retriver ###
+    # Rag component will grab the last 24 hours of tweets for the given ticker
+    # Rag component will grab the last 24 hours of news for the given ticker
 
-### Summarizer gpt4 ###
-# Summarizer component will summarize the news and tweets for the given ticker
-# get_summary(tickers)
+    ### Summarizer gpt4 ###
+    # Summarizer component will summarize the news and tweets for the given ticker
+    news_batch, tweets_batch = get_summary(tickers)
 
+    news_jsonl = tweets_jsonl = None
+    # we will wait until the batch finishes or times out after 30 seconds
+    with st.spinner("Generating Summaries. Please wait."):
+        get_batches().clear()
+        i = 0
+        time_limit = 30
+        while i < time_limit:
+            news_status, news_jsonl = retrieve_batch_summarize(news_batch.id)
+            tweets_status, tweets_jsonl = retrieve_batch_summarize(tweets_batch.id)
+
+            if news_status == SummaryStatus.COMPLETED and tweets_status == SummaryStatus.COMPLETED:
+                # we will now pass this on to the next part
+                break
+            # sleep for one second
+            time.sleep(1)
+            i += 1
+
+    if news_jsonl is None and tweets_jsonl is None:
+        st.write("It seems it is still generating. Please click retry or submit new tickers.")
+    else:
+        st.write(news_jsonl)
+        st.write(tweets_jsonl)
 ### Movement Predictor ###
 # ML movel will predict the movement of the stock price for the given ticker based on the numerical data from the rag component
 
@@ -53,3 +106,5 @@ if is_valid:
     st.write(f"Prediction for ticker: {tickers}")
 else:
     st.write("Invalid syntax.")
+
+main()
--- src/functions/scraper/Ticker.py
+++ src/functions/scraper/Ticker.py
@@ -1,32 +1,30 @@
-import datetime
 import os
+import pandas as pd
 import requests
 
 import requests
+import yfinance as yf
+from datetime import datetime
 from devtools import pprint
-from dotenv import load_dotenv
 from typing import List, Dict
 from finviz.main_func import get_stock
 
-load_dotenv()
 
-
-def get_float(stocks: List[str], days: List[datetime.datetime]) -> Dict[datetime.datetime, Dict[str, int]]:
+def get_recent_time_series(stocks: List[str]) -> Dict[str, pd.DataFrame]:
     """
     Returns the float values by day for each stock
 
     Stocks must be valid stocks. Will result in an error if the name of the stock does not exist
     :param stocks: a list of stock symbols
-    :param days: A list of days
+
     :return: The float values for each stock for each day
     """
     # For now, we will just see if it works for getting the float for today only
-    stock = get_stock("AAPL")
-    pprint(stock)
+    tickers = yf.Tickers(" ".join(stocks))
+
+    return {ticker: ticker_obj.history(period="1mo") for ticker, ticker_obj in tickers.tickers.items()}
 
 
 if __name__ == '__main__':
-    stocks = ["MSFT"]
-    days = [datetime.datetime.now()]
-
-    get_float(stocks, days)
+    stocks = ["MSFT", "GOOG"]
+    get_recent_time_series(stocks)
